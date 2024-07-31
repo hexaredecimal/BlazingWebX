f802cf00ae86d05f92cfcbe3522fff8ca991cbbc
hexaredecimal <mfanakagama@gmail.com>
2024-07-31 09:52:01 +0200

    genz: Adds a Html node for wrapping other nodes inside

--- src/genz/Html.java
+++ src/genz/Html.java
@@ -1,12 +1,56 @@
 package genz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hexaredecimal
  */
-public class Div extends GenZContainerElement {
+public class Html extends GenZContainerElement {
+
+	private List<String> top;
+	private List<String> bottom;
+
+	public Html() {
+		this.top = new ArrayList<>();
+		this.bottom = new ArrayList<>();
+	}
+
+	public Html title(String title) {
+		this.top.add(String.format("<title>%s</title>", title));
+		return this;
+	}
+	
+	public Html addHeaderScript(String url) {
+		this.top.add(String.format("<script type=\"text/javascript\" src=\"%s\"> </script>", url));
+		return this;
+	}
+
+	public Html addHeaderStyleLink(String url) {
+		this.top.add(String.format("<link rel=\"stylesheet\" url=\"%s\"> </script>", url));
+		return this;
+	}
+
 	@Override
 	public String render() {
-		return String.format("<div %s>\n%s</div>", attributes.render(), super.render());
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<html>".indent(0));
+		sb.append("<head>".indent(4));
+
+		top.forEach(element -> {
+			sb.append(element);
+		});
+
+		sb.append("</head>".indent(4));
+		sb.append("<body>".indent(4));
+		sb.append(super.render().indent(6));
+		bottom.forEach(element -> {
+			sb.append(element);
+		});
+		sb.append("</body>".indent(4));
+		sb.append("</html>".indent(0));
+
+		return sb.toString();
 	}
 }
