3dc5c088f97f2620c9166082c2b50ff99f15b61e
hexaredecimal <mfanakagama@gmail.com>
2024-07-31 09:53:30 +0200

    genz: Shorten the name of addAttribute to attr

--- src/genz/GenZElement.java
+++ src/genz/GenZElement.java
@@ -7,28 +7,28 @@ package genz;
 public abstract class GenZElement {
 	protected GenZAttributes attributes = new GenZAttributes(); 
 
-	public GenZElement addAttribute(String key, String value) {
+	public GenZElement attr(String key, String value) {
 		this.attributes.addAttribute(key, value);
 		return this; 
 	}
 
 	public GenZElement hxPost(String path) {
-		this.addAttribute("hx-post", path);
+		this.attr("hx-post", path);
 		return this;
 	}
 
 	public GenZElement hxGet(String path) {
-		this.addAttribute("hx-get", path);
+		this.attr("hx-get", path);
 		return this;
 	}
 
 	public GenZElement id(String id) {
-		this.addAttribute("id", id);
+		this.attr("id", id);
 		return this;
 	}
 	
 	public GenZElement className(String names) {
-		this.addAttribute("class", names);
+		this.attr("class", names);
 		return this;
 	}
 	public abstract String render(); 
