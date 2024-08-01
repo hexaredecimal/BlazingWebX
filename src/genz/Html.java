6e02ef7e3b35459d772ce4e60276297d3162cd6c
hexaredecimal <mfanakagama@gmail.com>
2024-08-01 19:35:59 +0200

    genz: Added htmx as a default script and fixed issues with the indent for the renderer

--- src/genz/Html.java
+++ src/genz/Html.java
@@ -15,6 +15,7 @@ public class Html extends GenZContainerElement {
 	public Html() {
 		this.top = new ArrayList<>();
 		this.bottom = new ArrayList<>();
+		this.addHeaderScript("https://unpkg.com/htmx.org@2.0.1");
 	}
 
 	public Html title(String title) {
@@ -28,7 +29,7 @@ public class Html extends GenZContainerElement {
 	}
 
 	public Html addHeaderStyleLink(String url) {
-		this.top.add(String.format("<link rel=\"stylesheet\" url=\"%s\"> </script>", url));
+		this.top.add(String.format("<link rel=\"stylesheet\" href=\"%s\" />", url));
 		return this;
 	}
 
@@ -38,15 +39,16 @@ public class Html extends GenZContainerElement {
 		sb.append("<html>".indent(0));
 		sb.append("<head>".indent(4));
 
+
 		top.forEach(element -> {
-			sb.append(element);
+			sb.append(element.indent(6));
 		});
 
 		sb.append("</head>".indent(4));
-		sb.append("<body>".indent(4));
+		sb.append(String.format("<body %s>", this.attributes.render()).indent(4));
 		sb.append(super.render().indent(6));
 		bottom.forEach(element -> {
-			sb.append(element);
+			sb.append(element.indent(6));
 		});
 		sb.append("</body>".indent(4));
 		sb.append("</html>".indent(0));
