633d914b341bb49275bcea0d7bb2d48a985718fe
hexaredecimal <mfanakagama@gmail.com>
2024-08-13 12:25:55 +0200

    fs: A new package for file system related functions

---
+++ src/blazing/fs/FileSystem.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blazing.fs;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class FileSystem {
+	public static String readFileToString(String path) {
+		try {
+			Scanner sc = new Scanner(new File(path));
+			StringBuilder sb = new StringBuilder(); 
+			while (sc.hasNextLine()) 
+				sb.append(sc.nextLine()).append("\n"); 
+
+			return sb.toString();
+		} catch (FileNotFoundException ex) {
+			return ex.getMessage();
+		}
+	}
+}
