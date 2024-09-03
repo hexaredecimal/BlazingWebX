58ba1fe5491ce3eff841f6114de9784755d356fc
hexaredecimal <mfanakagama@gmail.com>
2024-09-03 18:40:45 +0200

    webx: The Script tag

--- src/webx/Script.java
+++ src/webx/Script.java
@@ -4,41 +4,39 @@ package webx;
  *
  * @author hexaredecimal
  */
-public class Img extends WebXElement {
-	private String path, alt; 
-
-	public Img() {
-		this.path = this.alt = ""; 
+public class Script extends WebXElement {
+	private String path, type; 
+	private String code; 
+	public Script() {
+		this.code = "";
+		this.path = "";
+		this.type = "text/javascript"; 
 	}
 	
-	public Img(String path) {
-		this.path = path;
-		this.alt = ""; 
-	}
-	
-	public Img(String path, String alt) {
-		this.path = path;
-		this.alt = alt;
+	public Script(String code) {
+		this.code = code;
+		this.path = "";
+		this.type = "text/javascript"; 
 	}
 
-	public Img src(String path) {
+	public Script src(String path) {
 		this.path = path; 
 		return this;
 	}
 
-	public Img alt(String alt) {
-		this.alt = alt; 
+	public Script type(String alt) {
+		this.type = alt; 
 		return this;
 	}
 	
 	@Override
 	public String render() {
-		if (this.path != null) 
+		if (!this.path.isEmpty()) 
 			this.attributes.addAttribute("src", this.path);
 		
-		if (this.alt != null)
-			this.attributes.addAttribute("alt", this.alt);
+		if (!this.type.isEmpty())
+			this.attributes.addAttribute("type", this.type);
 		
-		return String.format("<img %s />", this.attributes.render());
+		return String.format("<script %s> %s </script>", this.attributes.render(), this.code.isEmpty() ? "" : this.code);
 	}
 }
