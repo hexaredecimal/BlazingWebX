dc905a4b14c0093b87769ebec9e744f06d24660d
hexaredecimal <mfanakagama@gmail.com>
2024-09-05 12:16:04 +0200

    blazing: Removed unused imports

--- src/blazing/fs/FileSystem.java
+++ src/blazing/fs/FileSystem.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package blazing.fs;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
