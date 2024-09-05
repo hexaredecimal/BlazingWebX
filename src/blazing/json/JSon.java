36bf2142d8faac47c9ab1cd5eded28bbc9f1b9c5
hexaredecimal <mfanakagama@gmail.com>
2024-09-05 12:17:10 +0200

    blazing: Some more work on the map to json module

--- src/blazing/json/JSon.java
+++ src/blazing/json/JSon.java
@@ -1,5 +1,7 @@
 package blazing.json;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,7 @@ public class JSon {
 	public static String from(Map<?, ?> map) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{".indent(0));
+		final String STANDARD_FMT = "\"%s\": %s%s";
 		var entries = map.entrySet().toArray();
 		for (int i = 0; i < entries.length; i++) {
 			var entry = (Map.Entry) entries[i];
@@ -20,15 +23,35 @@ public class JSon {
 			String end = i < entries.length - 1 ? "," : "";
 			String line = String.format("\"%s\": \"%s\"%s", key, value, end);
 
-			if (value instanceof Double || value instanceof Integer) {
-				line = String.format("\"%s\": %s%s", key, value, end);
+			if (value.getClass().isArray()) {
+				int length = Array.getLength(value);
+				Object[] boxedArray = new Object[length];
+				for (int j = 0; j < length; j++) {
+					boxedArray[j] = Array.get(value, j);
+				}
+				line = String.format(STANDARD_FMT, key, Arrays.toString(boxedArray), end);
+			} else if (value instanceof Double
+				|| value instanceof Integer
+				|| value instanceof Long
+				|| value instanceof Short
+				|| value instanceof Byte) {
+				line = String.format(STANDARD_FMT, key, value, end);
 			} else if (value instanceof Map<?, ?> child) {
 				value = JSon.from(child).trim();
-				line = String.format("\"%s\": %s%s", key, value, end);
+				line = String.format(STANDARD_FMT, key, value, end);
 			}
 			sb.append(line.indent(4));
 		}
 		sb.append("}".indent(0));
 		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		HashMap<Object, Object> map = new HashMap<>();
+		map.put("arr", new int[]{1, 2, 3, 4, 5});
+		map.put("arr2", new int[]{4, 5, 6, 2, 9, 7});
+
+		map.put("char", 'A');
+		System.out.println(from(map));
 	}
 }
