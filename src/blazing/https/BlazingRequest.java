daf936bcf4667495fceaa5c6fc1fbedd6060e6fe
hexaredecimal <mfanakagama@gmail.com>
2024-10-03 17:09:43 +0200

    https: Return an error result when a non 200 status is returned form any request type

--- src/blazing/https/BlazingRequest.java
+++ src/blazing/https/BlazingRequest.java
@@ -19,11 +19,11 @@ import java.io.IOException;
  * @author ERC
  */
 public class BlazingRequest {
-  
-  public static Result<String, IOException> post(String url, Map<?, ?> args) {
-    return doMethodWithBody("POST", url, args);
-  }
-	
+
+	public static Result<String, IOException> post(String url, Map<?, ?> args) {
+		return doMethodWithBody("POST", url, args);
+	}
+
 	public static Result<String, IOException> put(String url, Map<?, ?> args) {
 		return doMethodWithBody("PUT", url, args);
 	}
@@ -31,7 +31,7 @@ public class BlazingRequest {
 	public static Result<String, IOException> delete(String url) {
 		return doMethodWithoutBody("DELETE", url);
 	}
-	
+
 	public static Result<String, IOException> get(String urlString) {
 		return doMethodWithoutBody("GET", urlString);
 	}
@@ -43,54 +43,60 @@ public class BlazingRequest {
 	public static Result<String, IOException> head(String urlString) {
 		return doMethodWithoutBody("HEAD", urlString);
 	}
-	
 
-  private static Result<String, IOException> doMethodWithBody(String method, String url, Map<?, ?> args) {
-    try {
-      var server = new URL(url);
-      URLConnection con = server.openConnection();
-      HttpURLConnection http = (HttpURLConnection) con;
-      http.setRequestMethod(method.toUpperCase());
-      http.setDoOutput(true);
-      String results = Query.from(args);
-      byte[] out = results.getBytes(StandardCharsets.UTF_8);
-      int length = out.length;
-      http.setFixedLengthStreamingMode(length);
+	private static Result<String, IOException> doMethodWithBody(String method, String url, Map<?, ?> args) {
+		try {
+			var server = new URL(url);
+			URLConnection con = server.openConnection();
+			HttpURLConnection http = (HttpURLConnection) con;
+			http.setRequestMethod(method.toUpperCase());
+			http.setDoOutput(true);
+			String results = Query.from(args);
+			byte[] out = results.getBytes(StandardCharsets.UTF_8);
+			int length = out.length;
+			http.setFixedLengthStreamingMode(length);
 
-      // Allow the user to set the content properties
-      http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-      http.connect();
+			// Allow the user to set the content properties
+			http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			http.connect();
 
-      OutputStream os = http.getOutputStream();
-      os.write(out);
+			OutputStream os = http.getOutputStream();
+			os.write(out);
 
-      // Read the response
-      int status = http.getResponseCode();
-      InputStream responseStream;
-      if (status >= 200 && status < 300) {
-        responseStream = http.getInputStream(); // Success
-      } else {
-        return Result.err(new IOException("Server response: " + status));
-      }
+			// Read the response
+			int status = http.getResponseCode();
+			InputStream responseStream;
 
-      BufferedReader in = new BufferedReader(new InputStreamReader(responseStream, StandardCharsets.UTF_8));
-      String inputLine;
-      StringBuilder response = new StringBuilder();
-      while ((inputLine = in.readLine()) != null) {
-        response.append(inputLine);
-      }
-      
-      in.close();
+			boolean isError = false;
+			if (status >= 200 && status < 300) {
+				responseStream = http.getInputStream(); // Success
+			} else {
+				isError = true;
+				responseStream = http.getErrorStream();
+			}
 
-      // Print the response
-      String res = response.toString();
-      http.disconnect();
-      return Result.ok(res);
+			BufferedReader in = new BufferedReader(new InputStreamReader(responseStream, StandardCharsets.UTF_8));
+			String inputLine;
+			StringBuilder response = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
 
-    } catch (IOException ex) {
-      return Result.err(ex);
-    }
-  }
+			in.close();
+
+			// Print the response
+			String res = response.toString();
+			http.disconnect();
+			if (isError) {
+				return Result.err(new IOException("Error(" + status + ") with message: " + res));
+			}
+			return Result.ok(res);
+
+		} catch (IOException ex) {
+			return Result.err(ex);
+		}
+	}
+
 	private static Result<String, IOException> doMethodWithoutBody(String method, String urlString) {
 		HttpURLConnection http = null;
 		try {
@@ -103,9 +109,11 @@ public class BlazingRequest {
 			int status = http.getResponseCode();
 			InputStream responseStream;
 
+			boolean isError = false;
 			if (status == HttpURLConnection.HTTP_OK || status == HttpURLConnection.HTTP_NO_CONTENT) {
 				responseStream = http.getInputStream();
 			} else {
+				isError = true;
 				responseStream = http.getErrorStream();
 			}
 
@@ -119,6 +127,11 @@ public class BlazingRequest {
 			in.close();
 			String res = response.toString();
 			http.disconnect();
+
+			if (isError) {
+				return Result.err(new IOException("Error(" + status + ") with message: " + res));
+			}
+
 			return Result.ok(res);
 
 		} catch (IOException ex) {
