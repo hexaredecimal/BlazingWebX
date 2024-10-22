ffbce9e571950f2a7eef43f8a9e8d3beb372c80f
hexaredecimal <mfanakagama@gmail.com>
2024-10-22 08:32:43 +0200

    webx: Adds the scarfold component used for pairing childnodes without a parent

---
+++ src/webx/Scarfold.java
@@ -0,0 +1,12 @@
+package webx;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class Scarfold extends WebXContainerElement {
+	@Override
+	public String render() {
+		return String.format("%s", super.render());
+	}
+}
