404b9b75090aa03c36b80b9f107c75dba1729f17
hexaredecimal <mfanakagama@gmail.com>
2024-10-24 18:56:23 +0200

    json: Adds documentation to the callback function

--- src/blazing/lambda/BlazingStreamFunction.java
+++ src/blazing/lambda/BlazingStreamFunction.java
@@ -8,5 +8,10 @@ import java.io.OutputStream;
  * @author hexaredecimal
  */
 public interface BlazingStreamFunction {
+	/**
+	 * Used as a callback function for when you want to stream a file to the client.
+	 * @param outstream
+	 * @param streamfile 
+	 */
 	public void stream(OutputStream outstream, File streamfile);
 }
