647714c1033c0622e17f757901daa168a651468c
hexaredecimal <mfanakagama@gmail.com>
2025-03-27 01:03:02 +0200

    webx: Finish the Scarfold class

--- src/webx/Scarfold.java
+++ src/webx/Scarfold.java
@@ -5,8 +5,14 @@ package webx;
  * @author hexaredecimal
  */
 public class Scarfold extends WebXContainerElement {
+	String contents;
+
+	public Scarfold(String contents) {
+		this.contents = contents;
+	}
+	
 	@Override
 	public String render() {
-		return String.format("%s", super.render());
+		return String.format("%s %s", contents, super.render());
 	}
 }
